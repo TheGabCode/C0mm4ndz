4eec27fc43851dc845abc3f3e057ab314ab5c2af
Joseph Gabriel D. Gonzales <gabgebgib@gmail.com>
2017-05-05 10:45:16 +0800

    added refs in date()

--- Commands code snippets in C/date.c
+++ Commands code snippets in C/date.c
@@ -5,4 +5,6 @@ int main() {
     time_t t = time(NULL);
     struct tm *tm = localtime(&t);
     printf("%s\n", asctime(tm));
+    
+    //refs: http://stackoverflow.com/questions/1442116/how-to-get-date-and-time-value-in-c-program
 }
